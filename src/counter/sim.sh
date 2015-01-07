ebf9af19863cafddec9fb02bdd8307f14d896183
Stefan Biereigel <stefan@biereigel.de>
2015-01-07 09:49:37 +0100

    add counter

---
+++ src/counter/sim.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+TIME=100us
+UNIT=counter_tb
+ghdl --clean
+ghdl -a $UNIT.vhd
+ghdl -e $UNIT
+ghdl -m $UNIT 
+
+echo "[ TIME SIMULATION ]";
+ghdl -r $UNIT --wave=output.ghw  --stop-time=$TIME
+gtkwave output.ghw gtkwave.sav
+ghdl --clean
