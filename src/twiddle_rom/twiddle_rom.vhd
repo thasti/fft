fdda8b66c0b3aea517019e8cfb03991291fef0a3
Stefan Biereigel <stefan@biereigel.de>
2015-01-10 10:45:37 +0100

    twiddle_rom: output the correct TFs

--- src/twiddle_rom/twiddle_rom.vhd
+++ src/twiddle_rom/twiddle_rom.vhd
@@ -1,9 +1,17 @@
--- sine / cosine unit
--- 
--- clk  	main clock input
--- arg		argument to sine and cosine
--- sin		sine output
--- cos		cosine output
+-- Twiddle Factor generator
+-- for a given N-element-FFT the Twiddle Factors for every stage can be computed
+--
+-- generics:
+--  exponent	exponent of the length of the FFT (N=8 -> exponent = 3 (2^3 = 8)
+--  inwidth	number of address pins (-> ROM length)
+--  outwidth	width of the twiddle factors
+--
+-- ports:
+--  clk  	main clock input
+--  ctl		enable/disable twiddle rom - use 1 + 0j when ctl=0 (rom disabled)
+--  arg		argument to sine and cosine
+--  sin		sine output (imaginary part of TF)
+--  cos		cosine output (real part of TF)
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -12,12 +20,14 @@ use ieee.math_real.all;
 
 entity twiddle_rom is
 	generic (
-		inwidth : integer := 12;
+		exponent: integer := 3;
+		inwidth : integer := 2;
 		outwidth: integer := 10
 	);
 
 	port (
 		clk	: in std_logic;
+		ctl	: in std_logic;
 		arg	: in std_logic_vector(inwidth-1 downto 0);
 		q_sin	: out std_logic_vector(outwidth-1 downto 0);
 		q_cos 	: out std_logic_vector(outwidth-1 downto 0)
@@ -25,7 +35,7 @@ entity twiddle_rom is
 end twiddle_rom;
 
 architecture rtl of twiddle_rom is
-	constant rom_length 	: integer := (2**(inwidth-1));
+	constant rom_length 	: integer := (2**inwidth);
 	constant rom_max 	: integer := 2**outwidth;
 
 	-- sine rom saves 1 quadrant of sine
@@ -33,19 +43,23 @@ architecture rtl of twiddle_rom is
 	type rom_t is array(0 to rom_length-1) of signed(outwidth-1 downto 0);
 	signal sin_rom 		: rom_t;
 	signal cos_rom 		: rom_t;
-	signal address		: std_logic_vector(inwidth-2 downto 0);
+	signal address		: std_logic_vector(inwidth-1 downto 0);
 
 	signal output_sin	: std_logic_vector(outwidth-1 downto 0);
 	signal output_cos	: std_logic_vector(outwidth-1 downto 0);
-	signal sign		: std_logic;
 begin
 
+	-- twiddle factors: W^k_N = exp(j*2*pi*k/N)
+	-- where k:
+	--  (0,1,..,N/2-1) for first stage (N/2 factors, exponent = inwidth-1)
+	--  (0,2,..,N/2-1) for second stage (N/4 factors, exponent = inwidth-2)
+	--  (0,4,..,N/2-1) for third stage (N/8 factors, exponent = inwidth-3)
 	table : for i in 0 to rom_length-1 generate
 		sin_rom(i) <= to_signed(integer(
-			sin(MATH_2_PI * real(i) / real(rom_length*2)) * (real(rom_max)/2.0-1.0)
+			sin(MATH_2_PI * real((2**(exponent-inwidth-1))*i) / real(2**exponent)) * (real(rom_max)/2.0-1.0)
 			), outwidth);
 		cos_rom(i) <= to_signed(integer(
-			cos(MATH_2_PI * real(i) / real(rom_length*2)) * (real(rom_max)/2.0-1.0)
+			cos(MATH_2_PI * real((2**(exponent-inwidth-1))*i) / real(2**exponent)) * (real(rom_max)/2.0-1.0)
 			), outwidth);
 	end generate;
 
@@ -53,16 +67,17 @@ begin
 	output_sin <= std_logic_vector(sin_rom(to_integer(unsigned(address))));
 	output_cos <= std_logic_vector(cos_rom(to_integer(unsigned(address))));
 
-	q_sin <=	output_sin when sign = '0' else
-			std_logic_vector(to_signed(0, output_sin'length) - signed(output_sin));
-	q_cos <=	output_cos when sign = '0' else
-			std_logic_vector(to_signed(0, output_sin'length) - signed(output_cos));
+	q_sin <=	output_sin when ctl = '1' else
+			(others => '0'); -- imaginary part = 0
+
+
+	q_cos <=	output_cos when ctl = '1' else
+			'0' & std_logic_vector(to_signed(-1, q_cos'length-1)); -- real part = +1
 
 	output : process
 	begin
 		wait until rising_edge(clk);
-		address <= arg(inwidth-2 downto 0);
-		sign <= arg(inwidth-1);
+		address <= arg(inwidth-1 downto 0);
 	end process;
 end rtl;
 
--- src/twiddle_rom/twiddle_rom_tb.vhd
+++ src/twiddle_rom/twiddle_rom_tb.vhd
@@ -7,18 +7,21 @@ end twiddle_rom_tb;
 
 architecture behav of twiddle_rom_tb is
 	signal clk	: std_logic := '0';
-	signal arg	: std_logic_vector(0 downto 0) := (others => '0');
+	signal ctl	: std_logic := '1';
+	signal arg	: std_logic_vector(1 downto 0) := (others => '0');
 	signal q_sin	: std_logic_vector(7 downto 0);
 	signal q_cos	: std_logic_vector(7 downto 0);
 
 begin
 	dut : entity work.twiddle_rom
 	generic map (
-		inwidth => 1,
+		exponent => 3,
+		inwidth => 2,
 		outwidth => 8
 	)
 	port map (
 		clk => clk,
+		ctl => ctl,
 		arg => arg,
 		q_sin => q_sin,
 		q_cos => q_cos
@@ -28,6 +31,6 @@ begin
 	process
 	begin
 		wait until rising_edge(clk);
-		arg <= std_logic_vector(unsigned(arg) + to_unsigned(1, 1));
+		arg <= std_logic_vector(unsigned(arg) + to_unsigned(1, 2));
 	end process;
 end behav;
